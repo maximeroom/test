4dbda6df3779349829a967a81ea62b68d2d9d069
maximeroom <maximeroom123@live.be>
2017-02-20 11:04:17 +0100

    route en controller created

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Choose your product</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+<h1>
+    Choose a product!
+</h1>
+<h2>
+    Click on the product you prefer.
+</h2>
+<p>
+    blabla
+</p>
+</body>
+</html>
